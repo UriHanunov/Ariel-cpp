2f54ec85f719672708e849f71d0131d2d317f024
Uri Hanunov <44783443+UriHanunov@users.noreply.github.com>
2019-03-19 20:08:29 +0200

    Update BasicCheck.sh

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -5,27 +5,24 @@ $exitCode=0
 cd $foldername
 make
 secssesfillMake=$?
-if [$secssesfillMake -ne 0]
-	then
-	echo  Compilation     Memory leaks      thread race
-			FAIL            FAIL              FAIL
-	exit 7
+if [$secssesfillMake -ne 0]; then
+echo  "Compilation     Memory leaks      thread race
+	FAIL            FAIL              FAIL"
+exit 7
 	
 valgrind --tool=memcheck --leak-check=full --error-exitcode=3 -q ./$executeble >  /dev/null 2>&1
 Memory=$?
-if [$Memory -eq 3] 
-	then
-	$exitCode=2
+if [$Memory -eq 3]; then
+$exitCode=2
 fi
 
 valgrind --tool=helgrind --error-exitcode=3 -q ./$executeble > /dev/null 2>&1
 Threads=$?
-if [$Threads -eq 3] 
-	then
-	(($exitCode++))
+if [$Threads -eq 3]; then
+(($exitCode++))
 fi	
 
 
-echo  Compilation      Memory leaks    thread race
-				PASS             $MEMO            $THRED
+echo  "Compilation      Memory leaks    thread race
+				PASS             $MEMO            $THRED"
 exit $exitCode
