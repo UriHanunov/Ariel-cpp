d89c55e806f2d3b137a9a701db4e934551df51ea
Uri Hanunov <44783443+UriHanunov@users.noreply.github.com>
2019-03-19 20:47:54 +0200

    Update BasicCheck.sh

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -14,18 +14,20 @@ if [[$secssesfillMake -gt 0]]; then
 	exit 7
 fi
 	
-valgrind --leak-check=full --error-exitcode=1 ./$2 &> temp.txt
+valgrind --tool=memcheck --leak-check=full --error-exitcode=3 -q ./$2 &> temp.txt
 Memory=$?
-valgrind --tool=helgrind --error-exitcode=1 ./$2 &> temp.txt
+valgrind --tool=helgrind --error-exitcode=3 -q ./$2 &> temp.txt
 Threads=$?
 if [[$Memory -gt 0]]; then
 	exitCode=2
+	$MEMO=FAIL
 fi
 if[[$Threads -gt 0]]; then
 	exitCode=$exitCode+1
+	$THRED=FAIL
 fi
 
 
-echo  Compilation      Memory leaks    thread race
-		PASS             $MEMO            $THRED
+echo  	Compilation      Memory leaks    thread race
+	PASS             $MEMO            $THRED
 exit $exitCode
